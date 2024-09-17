55982614bb6093f26f71ef5307aa8fa3905ba3df
Voltage <poyaVoltage148@gmail.com>
2024-09-17 13:25:27 +0330

    Add glfw as seprate project

--- premake5.lua
+++ premake5.lua
@@ -5,6 +5,8 @@ workspace "TrexEngine"
 
 	OutputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+
+-----------------------------------------------------------------TrexEngine--------------------------------------------
 	project "TrexEngine"
 		location "TrexEngine"
 
@@ -21,11 +23,12 @@ workspace "TrexEngine"
 
 		includedirs {
 			"./TrexEngine/Source/Trex/Include",
-			"./TrexEngine/Source/Trex/Include/GL",
+			"./glfw/include/GLFW",
 			"./TrexEngine/Source/Trex/Include/GLFW",
 		}
 
 		libdirs {
+			("./bin/"..OutputDir.."/glfw"),
 			"./bin/External"
 		}
 
@@ -39,51 +42,77 @@ workspace "TrexEngine"
 			systemversion "10.0.17763.0"
 		
 			
-			filter ("configurations:Debug")
+		filter ("configurations:Debug")
 			symbols "On"
 			defines {"DEBUG"}
-			filter ("configurations:Release")
+		filter ("configurations:Release")
 			defines {"RELEASE"}
 			optimize "On"
 			
-			filter {"configurations:Debug", "system:windows"}
+		filter {"configurations:Debug", "system:windows"}
 			buildoptions {"/MDd"}
 			
+--------------------------------------------------GLFW-------------------------------------------------------
+project "glfw3"
+location "glfw3"
+kind "StaticLib"
+language "C"
+
+targetdir ("./bin/" .. OutputDir .. "/glfw3")
+objdir ("./bin-in/" .. OutputDir .. "/glfw3")
+
+files {
+	"./glfw/include/**.h",
+	"./glfw/src/**.c"
+}
+
+filter ("system:windows")
+	defines {"_WIN32", "_GLFW_WIN32"}
+	systemversion "10.0.17763.0"
+	staticruntime "On"
+
+filter("configurations:Debug")
+	symbols "On"
+filter("configurations:Release")
+	optimize "On"
+
+
+-------------------------------------------------SandBox----------------------------------------------
+
+	project "SandBox"
+		location "SandBox"
 			
-			project "SandBox"
-			location "SandBox"
+		kind "ConsoleApp"
+		language "C++"
 			
-			kind "ConsoleApp"
-			language "C++"
+		targetdir ("./bin/" .. OutputDir .. "/SandBox")
+		objdir ("./bin-in/" .. OutputDir .. "/SandBox")
 			
-			targetdir ("./bin/" .. OutputDir .. "/SandBox")
-			objdir ("./bin-in/" .. OutputDir .. "/SandBox")
+		files {
+			"./SandBox/Source/**.h",
+			"./SandBox/Source/**.cpp"
+		}
 			
-			files {
-				"./SandBox/Source/**.h",
-				"./SandBox/Source/**.cpp"
-			}
+		includedirs {
+			"./TrexEngine/Source/",
+			"./TrexEngine/Source/Trex/Include"
+		}
 			
-			includedirs {
-				"./TrexEngine/Source/",
-				"./TrexEngine/Source/Trex/Include"
-			}
+		libdirs {
+			("./bin/" .. OutputDir)
+		}
 			
-			libdirs {
-				("./bin/" .. OutputDir)
-			}
+		links {
+			"TrexEngine"
+		}
 			
-			links {
-				"TrexEngine"
-			}
-			
-			filter ("system:windows")
-			cppdialect "C++17"
-			staticruntime "On"
-			systemversion "10.0.17763.0"
-			defines {"PLATFORM_WINDOWS"}
-			postbuildcommands {"{COPY} ../bin/" .. OutputDir .. "/TrexEngine/TrexEngine.dll ../bin/" .. OutputDir .. "/SandBox",
-							   "{COPY} ../bin/External/vcruntime140_1d.dll ../bin/" .. OutputDir .. "/SandBox"}
+		filter ("system:windows")
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17763.0"
+		defines {"PLATFORM_WINDOWS"}
+		postbuildcommands {"{COPY} ../bin/" .. OutputDir .. "/TrexEngine/TrexEngine.dll ../bin/" .. OutputDir .. "/SandBox",
+						   "{COPY} ../bin/External/vcruntime140_1d.dll ../bin/" .. OutputDir .. "/SandBox"}
 			
 		filter ("configurations:Debug")
 			symbols "On"
